cece4cb6094fc34135f0494bbf34af866dba5e34
Caio Santos <caio.santos@msn.com>
2023-03-13 15:22:45 +0900

    add the actions and mutations

--- src/components/FilterTodos.vue
+++ src/components/FilterTodos.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
     <div>
-      <h1>Filter Todos</h1>
+      Filter Todos:
+      <select v-on:change="filterTodos($event)">
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+        <option selected value="200">200</option>
+      </select>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   name: 'FilterTodos',
+  methods: {
+    ...mapActions(['filterTodos']),
+  }
 }
 </script>
 
 <style scoped>
-
+  select {
+    margin-top: 20px;
+    padding: 10px;
+    border: 1px solid #41b883;
+  }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -13,8 +13,31 @@ const getters = {
 
 const actions = {
   async fetchTodos({ commit }) {
-    // using axios to get the data from the API
+    // using axios to get the data
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    // commit the mutation to set the state
+    commit('setTodos', response.data);
+  },
+  async deleteTodo({ commit }, id) {
+    // using axios to delete the data
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    commit('removeTodo', id);
+  },
+  async updateTodo({ commit }, updatedTodo) {
+    // using axios to update the data
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updatedTodo.id}`, updatedTodo);
+    commit('updateTodo', response.data);
+  },
+  async addTodo({ commit }, title) {
+    // using axios to add the data
+    const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false });
+    commit('addTodo', response.data);
+  },
+  async filterTodos({ commit }, e) {
+    // Get selected number
+    const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+    // using axios to get the data
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
     // commit the mutation to set the state
     commit('setTodos', response.data);
   }
@@ -24,6 +47,18 @@ const mutations = {
   // setting the state
   setTodos: (state, todos) => {
     state.todos = todos;
+  },
+  removeTodo: (state, id) => {
+    state.todos = state.todos.filter((todo) => todo.id !== id);
+  },
+  updateTodo: (state, updatedTodo) => {
+    const index = state.todos.findIndex(todo => todo.id === updatedTodo.id);
+    if (index !== -1) {
+      state.todos.splice(index, 1, updatedTodo);
+    }
+  },
+  addTodo: (state, newTodo) => {
+    state.todos.unshift(newTodo);
   }
 };
 
